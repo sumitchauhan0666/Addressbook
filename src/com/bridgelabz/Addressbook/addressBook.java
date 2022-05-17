2bdd7fa5539c7c3bde2a330cebdc9b705142b2df
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-17 22:17:21 +0530

    Added : Welcome Message

---
+++ src/com/bridgelabz/Addressbook/addressBook.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Addressbook;
+
+public class addressBook {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Addressbook ");
+    }
+
+
+}
