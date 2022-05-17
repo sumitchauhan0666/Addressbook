ab91386712f09e4489a0277ee81f6bef148a2a9a
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-17 22:29:03 +0530

    UPDATED: add a contact

--- src/com/bridgelabz/Addressbook/Contact.java
+++ src/com/bridgelabz/Addressbook/Contact.java
@@ -14,6 +14,7 @@ public class Contact {
     }
     @Override
     public String toString() {
+
         return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
                 + state + ", email=" + email + ", zip=" + zip + ", phoneNumber=" + phoneNumber + "]";
     }
--- src/com/bridgelabz/Addressbook/Person.java
+++ src/com/bridgelabz/Addressbook/Person.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.Addressbook;
 
 import java.util.Scanner;
-
 public class Person {
+
+    static Scanner sc = new Scanner(System.in);
+    static String firstName,lastName,address,state,email;
+    static int zip;
+    static long phoneNumber;
     static Contact [] personDetails = new Contact[10];
+
 
     public static void main(String args[]) {
 
@@ -13,10 +18,40 @@ public class Person {
         personDetails[0]=person1;
         personDetails[1]=person2;
         personDetails[2]=person3;
-
         System.out.println("Created contact list is");
         for(int i = 0; i < 3;i++) {
             System.out.println(personDetails[i]);
+        }
+        operations();
+    }
+    public static void operations() {
+        int j = 0;
+        System.out.println("Enter your choice");
+        System.out.println("1)Add contact");
+        int choice = sc.nextInt();
+        switch(choice) {
+            case 1:
+                System.out.println("Enter details of person");
+                System.out.println("Add first name");
+                firstName = sc.next();
+                System.out.println("Add last name");
+                lastName= sc.next();
+                System.out.println("Add address");
+                address = sc.next();
+                System.out.println("Add state");
+                state= sc.next();
+                System.out.println("Add zip");
+                zip = sc.nextInt();
+                System.out.println("Add phone number");
+                phoneNumber = sc.nextLong();
+                System.out.println("Add email");
+                email = sc.next();
+                Contact person4 = new Contact(firstName,lastName,address,state,zip,phoneNumber,email);
+                personDetails[3]=person4;
+                j++;
+                for(int i = 0; i < 3 + j;i++) {
+                    System.out.println(personDetails[i]);
+                }
         }
     }
 }
