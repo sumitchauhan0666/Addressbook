1ffaea3b2880a9fbfaaa9f020cd013f81586dbbf
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-17 23:14:36 +0530

    UPDATED: ability to search person

--- src/com/bridgelabz/Addressbook/AddressBookMain.java
+++ src/com/bridgelabz/Addressbook/AddressBookMain.java
@@ -8,9 +8,9 @@ public class AddressBookMain {
         person1.editContact();
         person1.deleteContact();
         person1.display();
-
         person1.addToBook();
         person1.operation();
         person1.displayBook();
+        person1.searchPerson();
     }
 }
